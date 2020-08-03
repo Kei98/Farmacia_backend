6ffc48482f9471fac1d5a02be42ff1a8d1d3beb2
Kei98 <keisychaves@hotmail.com>
2020-08-03 03:49:09 -0600

    [Migraciones de todas las tablas]

--- src/database/migrations/2020_08_03_075507_create_empresa_provs_table.php
+++ src/database/migrations/2020_08_03_075507_create_empresa_provs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMedicamentosTable extends Migration
+class CreateEmpresaProvsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMedicamentosTable extends Migration
      */
     public function up()
     {
-        Schema::create('medicamentos', function (Blueprint $table) {
+        Schema::create('empresa_provs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateMedicamentosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('medicamentos');
+        Schema::dropIfExists('empresa_provs');
     }
 }
